cd755e4466175c09a714651dde0e59c82bdb3cb8
Rosanna Claire Barnard <44380386+rosannaclairebarnard@users.noreply.github.com>
2020-08-03 22:37:10 +0100

    Update testing-RCB.R

--- R/testing-RCB.R
+++ R/testing-RCB.R
@@ -1,6 +1,7 @@
 # testing-RCB.R
 
 # don't forget you need library(covidm) the first time of use, after installing the covidm package
+library(covidm)
 
 # covidm options (for Windows)
 cm_path = "C:/Users/Rosanna/Documents/GitHub/covidm-alpha"; ### CHANGE THIS to reflect the path to covidm-alpha.
@@ -25,7 +26,7 @@ params = cm_parameters_SEI3R(cm_uk_locations("UK", 2), deterministic = T);
 # run the model
 run = cm_simulate(params, 1)
 
-# show results (note that ggplot package is not available for R version 4.0.2)
+# show results
 ggplot(run$dynamics[compartment == "cases"]) +
   geom_line(aes(t, value, colour = group, group = group)) + 
   facet_wrap(~population)
